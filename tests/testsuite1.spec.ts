885f5fd31bf901512d8917263340fce85e46f1c3
Andrey Yakovlev <vahejguru@outlook.com>
2024-09-09 17:03:26 +0200

    testsuite1 debugged assertion for logout

--- tests/testsuite1.spec.ts
+++ tests/testsuite1.spec.ts
@@ -2,13 +2,13 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Test Suite 1', () => {
  test('Test Case 1', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('http://localhost:3000/login');
   
   //login & assertions:
-  await page.goto('http://localhost:3000');
+  await page.goto(`${process.env.BASE_URL}`);
   await expect(page.getByRole('link', { name: 'Tester Hotel' })).toBeVisible(); //assertion
-  await page.locator('input[type="text"]').fill('tester01');
-  await page.locator('input[type="password"]').fill('GteteqbQQgSr88SwNExUQv2ydb7xuf8c');
+  await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+  await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
   // logout:

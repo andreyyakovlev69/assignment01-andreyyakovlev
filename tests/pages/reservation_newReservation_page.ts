2c381e92cd763a6448b3528d02335ebb59e5bf11
Andrey Yakovlev <vahejguru@outlook.com>
2024-09-24 12:29:56 +0200

    edit reservation_newReservation_page attributes

--- tests/pages/reservation_newReservation_page.ts
+++ tests/pages/reservation_newReservation_page.ts
@@ -3,18 +3,22 @@ import { expect, type Locator, type Page } from '@playwright/test';
 export class NewReservationPage {
   readonly page: Page;
   readonly viewButton: Locator;
-  readonly createReservationButton: Locator;
-  readonly editReservationMenu: Locator;
-  readonly editReservationButton: Locator;
+  readonly startField: Locator;
+  readonly endField: Locator;
+  readonly clientField: Locator;
+  readonly roomField: Locator;
+  readonly billField: Locator;
   readonly backButton: Locator;
   readonly logoutButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.viewButton = page.locator('#app > div > div > div:nth-child(4) > a');
-    this.createReservationButton = page.locator('#app > div > h2 > a');
-    this.editReservationMenu = page.getByText('Edit');
-    this.editReservationButton = page.locator('#app > div > div.reservations > div > div.action > img');
+    this.startField = page.locator('#app > div > div:nth-child(2) > div:nth-child(1) > input[type=text]');
+    this.endField = page.locator('#app > div > div:nth-child(2) > div:nth-child(2) > input[type=text]');
+    this.clientField = page.locator('#app > div > div:nth-child(2) > div:nth-child(3) > select > option:nth-child(2)');
+    this.roomField = page.locator('#app > div > div:nth-child(2) > div:nth-child(4) > select > option:nth-child(2)');
+    this.billField = page.locator('#app > div > div:nth-child(2) > div:nth-child(5) > select > option:nth-child(2)');
     this.backButton = page.locator('#app > div > div:nth-child(3) > a');
     this.logoutButton = page.locator('#app > header > div > div > button');
   }
@@ -23,15 +27,7 @@ export class NewReservationPage {
   async performView() {
     await this.viewButton.click();
   };
-  async performCreateReservation() {
-    await this.createReservationButton.click();
-  };
-  async performEditReservationButton() {
-    await this.editReservationButton.click();
-  };
-  async performEditReservationMenu() {
-    await this.editReservationMenu.click();
-  };
+  
   async performBackButton() {
     await this.backButton.click();
   };

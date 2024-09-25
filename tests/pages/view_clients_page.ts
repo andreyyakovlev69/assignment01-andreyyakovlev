84e6187be6fe6dbd6dd6dcac1b6e881725e3573f
Andrey Yakovlev <vahejguru@outlook.com>
2024-09-25 10:05:15 +0200

    edit view_clients_page with edit&delete methods

--- tests/pages/view_clients_page.ts
+++ tests/pages/view_clients_page.ts
@@ -4,7 +4,7 @@ export class ViewClientsPage {
   // Declaring class properties for the page and various buttons:
   readonly page: Page;
   readonly viewButton: Locator;
-  readonly editClientField: Locator;
+  readonly editClientMenu: Locator;
   readonly deleteClientButton: Locator;
   readonly editClientButton: Locator;
   readonly logoutButton: Locator;
@@ -15,7 +15,7 @@ export class ViewClientsPage {
   constructor(page: Page) {
     this.page = page;
     this.viewButton = page.locator('#app > div > div > div:nth-child(2) > a');
-    this.editClientField = page.locator('#app > div > div.clients > div:nth-child(1) > div.action > img');
+    this.editClientMenu = page.locator('#app > div > div.clients > div:nth-child(1) > div.action > img');
     this.editClientButton = page.locator('#app > div > div.clients > div:nth-child(1) > div.menu > a:nth-child(1)');
     this.deleteClientButton = page.locator('#app > div > div.clients > div:nth-child(1) > div.menu > a:nth-child(2)');
     this.logoutButton = page.locator('#app > header > div > div > button');
@@ -28,7 +28,15 @@ export class ViewClientsPage {
   async performView() {
     await this.viewButton.click();
   };
-
+  async performEditClientMenu() {
+    await this.editClientMenu.click();
+  };
+  async performEditClientButton() {
+    await this.editClientButton.click();
+  };
+  async performDeleteClientButton() {
+    await this.deleteClientButton.click();
+  };
   // Method to perform a click action on the 'Create Client' button:
   async performCreateClientButton() {
     await this.createClientButton.click();

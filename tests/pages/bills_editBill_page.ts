d97c51cd8517899ed63e013a18f778e24b7a8687
Andrey Yakovlev <vahejguru@outlook.com>
2024-09-25 11:55:09 +0200

    edit bills_editBill_page

--- tests/pages/bills_editBill_page.ts
+++ tests/pages/bills_editBill_page.ts
@@ -1,3 +1,4 @@
+import { th } from '@faker-js/faker';
 import { expect, type Locator, type Page } from '@playwright/test';
 
 export class BillsPage {
@@ -7,6 +8,7 @@ export class BillsPage {
   readonly valueField: Locator;
   readonly paidField: Locator;
   readonly saveButton: Locator;
+  readonly deleteButton: Locator;
   readonly logoutButton: Locator;
   readonly backButton: Locator;
 
@@ -17,16 +19,16 @@ export class BillsPage {
     this.valueField = page.locator('#app > div > div:nth-child(2) > div:nth-child(1) > input[type=number]');
     this.paidField = page.locator('#app > div > div:nth-child(2) > div:nth-child(2) > div');
     this.saveButton = page.locator('#app > div > div.actions > a.btn.blue');
+    this.deleteButton = page.locator('#app > div > h2 > a');
     this.logoutButton = page.locator('#app > header > div > div > button');
     this.backButton = page.locator('#app > div > div.actions > a:nth-child(1)')
   }
 
   //Methods:
-  // Method to perform a click action on the 'View' button:
+  // Method to perform an action on the '...' button:
   async performView() {
     await this.viewButton.click();
   };
-
   async fillValueField(randomPrice: string) {
     await this.valueField.fill(randomPrice);
   };
@@ -35,6 +37,12 @@ export class BillsPage {
   };
   async performBackButton() {
     await this.backButton.click();
+  };
+  async performSaveButton() {
+    await this.saveButton.click();
+  };
+  async performDeleteButton() {
+    await this.deleteButton.click();
   };
   async performLogoutButton() {
     await this.logoutButton.click();
